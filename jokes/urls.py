31baec8ace22aa2f567721bd7b6cddccb6c55775
Pieter Walraven <p.walraven28@gmail.com>
2023-02-23 12:19:39 +0100

    commit 23-2

--- jokes/urls.py
+++ jokes/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import JokeListView
 
 app_name = 'jokes'
-urlpatterns = [
-    path('', JokeListView.as_view(), name='list')
-]
+urlpatterns = []
+   
     
